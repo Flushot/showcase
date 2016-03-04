0228db0c05c5d0befd30267861a201e38cf800ad
Chris Lyon <chrislyon@tradesy.com>
2016-03-04 15:38:20 -0800

    replaced express with restify and added falcor-router

--- renderServer.js
+++ renderServer.js
@@ -1,14 +1,76 @@
-var express = require('express');
-var app = express();
-
+var restify = require('restify');
+var bunyan = require('bunyan');
+var falcorRestifyMiddleware = require('falcor-restify');
+var FalcorRouter = require('falcor-router');
 
 var listenPort = 3002;
 
-app.get('/', function(request, response) {
-    response.send('Render server is alive.');
+var log = bunyan.createLogger({
+    name: 'renderServer',
+    level: bunyan.DEBUG,
+    src: true
 });
 
-app.post('/render', function(request, response) {
+var server = restify.createServer({
+    log: log.child({
+        component: 'server',
+        level: bunyan.INFO,
+        streams: [
+            {
+                // This ensures that if we get a WARN or above all debug records
+                // related to that request are spewed to stderr - makes it nice
+                // filter out debug messages in prod, but still dump on user
+                // errors so you can debug problems
+                level: bunyan.DEBUG,
+                type: 'raw',
+                stream: new restify.bunyan.RequestCaptureStream({
+                    level: bunyan.WARN,
+                    maxRecords: 100,
+                    maxRequestIds: 1000,
+                    stream: process.stderr
+                })
+            }
+        ],
+        serializers: bunyan.stdSerializers
+    })
+});
+
+server.use(restify.requestLogger());
+server.use(restify.queryParser());
+
+server.on('after', restify.auditLogger({
+    log: log.child({
+        component: 'audit'
+    })
+}));
+server.on('uncaughtException', function (req, res, route, err) {
+    req.log.error(err, 'got uncaught exception');
+});
+
+
+server.get('/hello/:name', function (request, response, next) {
+    response.send('Hello ' + request.params.name);
+    next();
+});
+
+server.get('/model.json', falcorRestifyMiddleware(function (request, response) {
+    // create a Virtual JSON resource with single key ("greeting")
+    return new FalcorRouter([
+        {
+            // match a request for the key "greeting"
+            route: 'greeting',
+            // respond with a PathValue with the value of "Hello World."
+            get: function () {
+                return {
+                    path: ['greeting'],
+                    value: 'Hello World'
+                };
+            }
+        }
+    ]);
+}));
+
+server.post('/render', function (request, response, next) {
     // TODO: require() compiled webpack scripts
     // TODO: set window.location to url passed in as param
     // TODO: set view state from state passed in as param (figure out a better way than with globals)
@@ -16,8 +78,13 @@ app.post('/render', function(request, response) {
     // TODO: determine if route is valid and throw a 404 if not
     // TODO: response.send() rendered html
     response.send('Not implemented');
+    next();
 });
 
-app.listen(listenPort, function() {
-    console.log('Render server is listening on port ' + listenPort);
+server.get(/.+/, restify.serveStatic({
+    directory: './public'
+}));
+
+server.listen(listenPort, function () {
+    console.log('%s listening at %s', server.name, server.url);
 });
